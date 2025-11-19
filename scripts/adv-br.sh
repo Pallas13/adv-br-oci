682a7de1dc9d487060e3442f61d033d275ec0a30
Pallas13 <edsondasilvaguedes@gmail.com>
2025-11-19 12:14:06 -0300

    Atualização para novas modificações do adv-br Imagem definida para Ubuntu 24.04 LTS.

---
+++ scripts/adv-br.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Symlink distrobox shims
+./distrobox-shims.sh
+
+# Update the container and install packages
+sudo apt update
+sudo apt upgrade
+sudo apt install curl -y
+sudo apt install $(cat ./adv-br.packages | tr "\n" " ") -y
+
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_safesign.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_safenet.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_serproid.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/scripts-ubuntu-24-04/instalar_assinadorserpro.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_java.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_pjeoffice.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_webpki.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_websigner.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_certisign.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/scripts-ubuntu-24-04/instalar-firefox.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/scripts-ubuntu-24-04/configurar_tokens_firefox.sh | sudo -u $USER sh
+
