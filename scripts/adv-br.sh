7cfe8c67067f0ca4cbde475f603615f704a2eaac
Pallas13 <edsondasilvaguedes@gmail.com>
2025-11-21 21:08:07 -0300

    Trocar de base para Debian 13

--- scripts/adv-br.sh
+++ scripts/adv-br.sh
@@ -9,15 +9,11 @@ apt upgrade
 apt install curl sudo wget -y
 apt install $(cat ./adv-br.packages | tr "\n" " ") -y
 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_safesign.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_safenet.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_serproid.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/scripts-ubuntu-24-04/instalar_assinadorserpro.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_java.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_pjeoffice.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_webpki.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_websigner.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_certisign.sh | sh 
-curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/scripts-ubuntu-24-04/instalar-firefox.sh | sh 
-#curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/scripts-ubuntu-24-04/configurar_tokens_firefox.sh | sh
-
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_safesign.sh | sh 
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_safenet.sh | sh 
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_serproid.sh | sh 
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_assinadorserpro.sh | sh 
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_java.sh | sh 
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_pjeoffice.sh | sh 
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_webpki.sh | sh 
+curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_websigner.sh | sh
