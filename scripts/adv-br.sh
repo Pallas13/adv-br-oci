ea5288c169b027c4baf149aec4890783ebeb552b
Pallas13 <edsondasilvaguedes@gmail.com>
2025-11-21 21:38:58 -0300

    Erro de repositorio do adv

--- scripts/adv-br.sh
+++ scripts/adv-br.sh
@@ -9,11 +9,11 @@ apt upgrade
 apt install curl sudo wget -y
 apt install $(cat ./adv-br.packages | tr "\n" " ") -y
 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_safesign.sh | sh 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_safenet.sh | sh 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_serproid.sh | sh 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_assinadorserpro.sh | sh 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_java.sh | sh 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_pjeoffice.sh | sh 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_webpki.sh | sh 
-curl https://raw.githubusercontent.com/pedrohqb/distrobox-adv-br/refs/heads/main/scripts/instalar_websigner.sh | sh
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_safesign.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_safenet.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_serproid.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_assinadorserpro.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_java.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_pjeoffice.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_webpki.sh | sh 
+curl https://raw.githubusercontent.com/Pallas13/distrobox-adv-br/refs/heads/main/scripts/instalar_websigner.sh | sh
